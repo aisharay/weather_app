f8c16b947fd50cab45d0ddd5e386da18a4eead85
Aisha ray <74696016+aisharay@users.noreply.github.com>
2022-01-14 22:22:22 +0530

    Update weather-widget-main.component.ts

--- src/app/mycomponent/weather-widget-main/weather-widget-main.component.ts
+++ src/app/mycomponent/weather-widget-main/weather-widget-main.component.ts
@@ -14,7 +14,7 @@ import { Component, OnInit } from '@angular/core';
       console.log(this.WeatherData); 
     } 
       
-      getWeatherData(){ fetch('https://api.openweathermap.org/data/2.5/weather?q=bhubaneswar&appid=ff1bc4683fc7325e9c57e586c20cc03e') .then(response=>response.json()) .then(data=>{this.setWeatherData(data);}) 
+      getWeatherData(){ fetch('https://api.openweathermap.org/data/2.5/weather?q=bhubaneswar&appid=325551cbca4ae78825ae77c3f0a397d1') .then(response=>response.json()) .then(data=>{this.setWeatherData(data);}) 
       // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}'); 
       // this.setWeatherData(data); 
     } 
